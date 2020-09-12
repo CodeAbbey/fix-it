47ecaa125670f3584ba3058eabed0005d28e0643
Rodion Gorkovenko <rodion_gork@mail.ru>
2020-09-12 22:12:26 +0300

    Update a001-round.py

--- py/a001-round.py
+++ py/a001-round.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
-while True:
-    try:
-        vals = [int(v) for v in input().split()]
-        if len(vals) != 2:
+def my_round(x):
+    """
+    Rounds to nearest integer, in case of exact half (3.5, 10.5 etc) rounds up
+    as taught in school
+    """
+    return round(x)
+
+def main():
+    """
+    Reads two values A and B from each line, outputs A / B rounded
+    """
+    while True:
+        try:
+            vals = [int(v) for v in input().split()]
+            if len(vals) != 2:
+                break
+            print(my_round(vals[0] / vals[1]))
+        except EOFError:
             break
-        print(round(vals[0] / vals[1]))
-    except EOFError:
-        break
+
+if __name__ == '__main__':
+    main()
